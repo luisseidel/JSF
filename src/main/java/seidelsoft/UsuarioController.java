a0d3c3e727def1c0bbbd0b58c69f023201b77b98
Luis Seidel <luis.seidel@celk.net>
2023-01-27 19:11:42 -0300

    fix: works

--- src/main/java/seidelsoft/UsuarioController.java
+++ src/main/java/seidelsoft/UsuarioController.java
@@ -4,12 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
+@SessionScoped
 @ManagedBean(name = "usuarioController")
 public class UsuarioController {
 
     private String name;
+    private List<String> nomes = new ArrayList<>();
+
+    public String addNome() {
+        nomes.add(name);
+        return "";
+    }
 
 }
